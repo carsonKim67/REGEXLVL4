63143ecebfdd89efef6ce566f2fd9ccf7a0a167f
carsonKim67 <carson.p.kim@gmail.com>
2026-09-14 15:58:59 +0000

    adsasdsadsad

--- Stuff.java
+++ Stuff.java
@@ -14,13 +14,42 @@ public class Stuff {
         URL url = new URL("https://www.pythonchallenge.com/pc/def/linkedlist.php?nothing=12345");
          BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
          String line = reader.readLine();
-         
+         int divTwo=0;
 
 
-         Pattern pat = Pattern.compile("[\\d]+"); // step 1 compile your pattern
+
+         Pattern pat = Pattern.compile("the next nothing is ([\\d]+)"); // step 1 compile your pattern
              Matcher mat = pat.matcher(line); //step 2 set up your matcher
              while(mat.find()) {//step 3 look through until you find a match
-                  line = mat.group();
+                  line = mat.group(1);
+                  divTwo = Integer.parseInt(line);
+                  url= new URL("https://www.pythonchallenge.com/pc/def/linkedlist.php?nothing="+line);
+                  reader = new BufferedReader(new InputStreamReader(url.openStream()));
+                  line = reader.readLine();
+                  System.out.println(line);
+                  mat = pat.matcher(line);
+             }
+             System.out.println(divTwo);
+             divTwo /=2;
+             url=new URL("https://www.pythonchallenge.com/pc/def/linkedlist.php?nothing="+divTwo);
+               reader = new BufferedReader(new InputStreamReader(url.openStream()));
+               line = reader.readLine();
+               mat = pat.matcher(line);
+               while(mat.find()) {//step 3 look through until you find a match
+                  line = mat.group(1);
+                  url= new URL("https://www.pythonchallenge.com/pc/def/linkedlist.php?nothing="+line);
+                  reader = new BufferedReader(new InputStreamReader(url.openStream()));
+                  line = reader.readLine();
+                  System.out.println(line);
+                  mat = pat.matcher(line);
+             }
+             System.out.println(reader.readLine());
+             url=new URL("https://www.pythonchallenge.com/pc/def/linkedlist.php?nothing="+divTwo);
+               reader = new BufferedReader(new InputStreamReader(url.openStream()));
+               line = reader.readLine();
+               mat = pat.matcher(line);
+               while(mat.find()) {//step 3 look through until you find a match
+                  line = mat.group(1);
                   url= new URL("https://www.pythonchallenge.com/pc/def/linkedlist.php?nothing="+line);
                   reader = new BufferedReader(new InputStreamReader(url.openStream()));
                   line = reader.readLine();
